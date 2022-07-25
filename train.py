523f7e5b05a1068aa861ca11765d648c6e49acdf
Viet Nhat Thai <thaivietnhat02@gmail.com>
2022-07-26 01:42:06 +0700

    update Tracker

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
 testloader = DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False)
 
 class Tracker:
-    def __init__(self, model_name, save_path):
+    def __init__(self, model_name, save_path, model=None):
         self.model_name = model_name
         self.save_path = save_path
         self.best_loss = float('inf')
@@ -52,39 +52,43 @@ class Tracker:
         os.makedirs(self.log_path, exist_ok=True)
         self.log_path = os.path.join(self.log_path, 'log.txt')
         if os.path.exists(self.log_path):
-            self.get_last_best()
+            self._get_last_best()
 
         self.log = open(self.log_path, 'a+')
 
         print("last epoch:", self.last_epoch)
         if self.last_best_epoch:
             print("last best epoch:", self.last_best_epoch)
+        if self.last_epoch and model is not None:
+            self._load_last_model(model)
 
     def update(self, epoch, loss, acc, model):
         if loss < self.best_loss:
-            self.write_log(epoch, loss, acc, is_best=True)
+            self._write_log(epoch, loss, acc, is_best=True)
             self.best_loss = loss
             self.best_acc = acc
             self.best_epoch = epoch
             print(f'    - best loss: {self.best_loss:.4f} acc: {self.best_acc:.4f} epoch: {self.best_epoch}')
-            self.save(model)
+            self._save(model)
         else:
-            self.write_log(epoch, loss, acc)
+            self._write_log(epoch, loss, acc)
 
-    def save(self, model):
+    def _save(self, model):
         model.save_pretrained(os.path.join(self.save_path, self.model_name, 'best'))
 
-    def load(self):
-        self.best_model.save_pretrained(os.path.join(self.save_path, self.model_name))
+    def _load_last_model(self, model):
+        if (os.path.exists(os.path.join(self.save_path, self.model_name, 'config.json')) 
+        and os.path.exists(os.path.join(self.save_path, self.model_name, 'pytorch_model.dat'))):
+            model.load_pretrained(os.path.join(self.save_path, self.model_name))
     
-    def write_log(self, epoch, loss, acc, is_best=False):
+    def _write_log(self, epoch, loss, acc, is_best=False):
         if not is_best:
             self.log.write(f'{epoch:4d} {loss:.4f} {acc:.4f}\n')
         else:
             self.log.write(f'{epoch:4d} {loss:.4f} {acc:.4f} *\n')
         self.log.flush()
 
-    def get_last_best(self):
+    def _get_last_best(self):
         with open(self.log_path, 'r') as f:
             lines = f.readlines()
             lines.reverse()
@@ -169,12 +173,10 @@ def eval(epoch, model, testloader, criterion, verbose=1):
 
     return torch.mean(torch.tensor(test_loss)), torch.mean(torch.tensor(test_acc))
 
-model.from_pretrained('weights/bert-reviews-kaggle')
-
 optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
 criterion = torch.nn.MSELoss()
 
-tracker = Tracker(MODELNAME, SAVE_PATH)
+tracker = Tracker(MODELNAME, SAVE_PATH, model)
 
 for epoch in range(tracker.get_epoch(), EPOCHS+1):
     train(epoch, model, trainloader, optimizer, criterion)
