d654e6c9e25218b92bec20141dc434ac9452ad21
Viet Nhat Thai <thaivietnhat02@gmail.com>
2022-07-25 16:37:51 +0700

    create evaluation

---
+++ evaluation.py
@@ -0,0 +1,49 @@
+import os
+from tqdm import tqdm
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+from dataset import ReviewDataset
+from model import ReviewsModel
+
+BATCH_SIZE = 128
+MAX_LENGTH = 128
+
+DATA_PATH = 'data'
+SAVE_PATH = 'weights'
+MODELNAME = 'bert-reviews'
+MODELNAME_OR_PATH = 'distilbert-base-uncased-finetuned-sst-2-english'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('device:', device)
+
+tokenizer = AutoTokenizer.from_pretrained(MODELNAME_OR_PATH)
+
+trainset = ReviewDataset(tokenizer, DATA_PATH, MAX_LENGTH, do_train=True)
+testset = ReviewDataset(tokenizer, DATA_PATH, MAX_LENGTH, do_train=False)
+
+model = ReviewsModel()
+model.from_pretrained(os.path.join(SAVE_PATH, MODELNAME))
+model.to(device)
+
+total = sum(p.numel() for p in model.parameters())
+print(f'total parameters: {total:,}')
+
+def eval_train(trainloader):
+    model.eval()
+    train_preds = []
+    with torch.no_grad():
+        for batch in tqdm(trainloader, desc='Eval train'):
+            batch = tuple(t.to(device) for t in batch)
+            input_ids, attention_mask, labels, ratings = batch
+            outputs = model(input_ids, attention_mask)
+            train_preds.extend(outputs.detach().cpu().numpy())
+    return train_preds
+
+trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=False)
+train_preds = eval_train(trainloader)
+
+# save train predictions
+with open('train_preds.txt', 'w') as f:
+    for pred in train_preds:
+        f.write(str(pred) + '\n')
